c423c6b9d3df25bf9b2a8b39ca3bcd07d856eb96
aayien-IITI <batmanpandit2005@gmail.com>
2025-07-09 12:34:00 +0530

    Hello line added

--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello niggesh");
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
